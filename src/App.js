2d541ca8ca3bf3d7a59b881632bec053eed4fecc
Gabriela Vargas <gvargas@sugvargas.mdlive.local>
2018-01-31 11:24:32 -0500

    added map component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
+import { Map } from './components/Map'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Map
-      </div>
+      <Map />
     );
   }
 }
